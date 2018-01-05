a50529000d760b9dc5af95ea92b695b57328501b
dianfs998 <yy.yanjl@gmail.com>
2018-01-05 18:16:42 +0800

    添加Resource

--- GameFrameWorkLearn/DataTable/IDataTableManager.cs
+++ GameFrameWorkLearn/DataTable/IDataTableManager.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using GameFrameWork.Resource;
 
 namespace GameFrameWork.DataTable
 {
@@ -12,23 +13,33 @@ namespace GameFrameWork.DataTable
         /// <summary>
         /// 加载数据表时加载依赖资源事件
         /// </summary>
-        event EventHandler<LoadDataTableDependencyAssetEventArgs> loadDataTableDependencyAsset;
+        event EventHandler<LoadDataTableDependencyAssetEventArgs> LoadDataTableDependencyAsset;
 
         /// <summary>
         /// 加载数据表成功事件
         /// </summary>
-        event EventHandler<LoadDataTableSuccessEventArgs> loadDataTableSuccess;
+        event EventHandler<LoadDataTableSuccessEventArgs> LoadDataTableSuccess;
 
         /// <summary>
         /// 加载数据表失败事件
         /// </summary>
-        event EventHandler<LoadDataTableFailureEventArgs> loadDataTableFailure;
+        event EventHandler<LoadDataTableFailureEventArgs> LoadDataTableFailure;
 
         /// <summary>
         /// 加载数据表更新事件
         /// </summary>
-        event EventHandler<LoadDataTableUpdateEventArgs> loadDataTableUpdate;
+        event EventHandler<LoadDataTableUpdateEventArgs> LoadDataTableUpdate;
 
-        //todo
+        /// <summary>
+        /// 设置资源管理器
+        /// </summary>
+        /// <param name="resourceManager">资源管理器</param>
+        void SetResourceManager(IResourceManager resourceManager);
+
+        /// <summary>
+        /// 设置
+        /// </summary>
+        /// <param name="dataTableHelper"></param>
+        void SetDataTableHelper(IDataTableHelper dataTableHelper);
     }
 }
--- GameFrameWorkLearn/Resource/IResourceManager.cs
+++ GameFrameWorkLearn/Resource/IResourceManager.cs
@@ -270,7 +270,7 @@ namespace GameFrameWork.Resource
         /// </summary>
         /// <param name="assetName">要加载的资源名称</param>
         /// <param name="loadAssetCallbacks">加载资源回调函数集</param>
-        void LoadResource(string assetName, LoadAssetCallbacks loadAssetCallbacks);
+        void LoadAsset(string assetName, LoadAssetCallbacks loadAssetCallbacks);
 
         /// <summary>
         /// 异步加载资源
@@ -278,12 +278,84 @@ namespace GameFrameWork.Resource
         /// <param name="assetName">要加载的资源名称</param>
         /// <param name="loadAssetCallbacks">加载资源回调函数集</param>
         /// <param name="userData">用户自定义数据</param>
-        void LoadResource(string assetName, LoadAssetCallbacks loadAssetCallbacks, object userData);
+        void LoadAsset(string assetName, LoadAssetCallbacks loadAssetCallbacks, object userData);
 
         /// <summary>
         /// 卸载资源
         /// </summary>
         /// <param name="asset">要卸载的资源</param>
         void UnloadAsset(object asset);
+
+        /// <summary>
+        /// 异步加载场景
+        /// </summary>
+        /// <param name="sceneAssetName">要加载场景资源的名称</param>
+        /// <param name="loadSceneCallbacks">加载场景回调函数集</param>
+        void LoadScene(string sceneAssetName, LoadSceneCallbacks loadSceneCallbacks);
+
+        /// <summary>
+        /// 异步加载场景
+        /// </summary>
+        /// <param name="sceneAssetName">要加载场景资源的名称</param>
+        /// <param name="loadSceneCallbacks">加载场景回调函数集</param>
+        /// <param name="userData">用户自定义数据</param>
+        void LoadScene(string sceneAssetName, LoadSceneCallbacks loadSceneCallbacks, object userData);
+
+        /// <summary>
+        /// 异步卸载场景
+        /// </summary>
+        /// <param name="sceneAssetName">要卸载的场景资源名称</param>
+        /// <param name="unloadSceneCallBacks">卸载场景回调函数集</param>
+        void UnloadScene(string sceneAssetName, UnloadSceneCallBacks unloadSceneCallBacks);
+
+        /// <summary>
+        /// 异步卸载场景
+        /// </summary>
+        /// <param name="sceneAssetName">要卸载的场景资源名称</param>
+        /// <param name="unloadSceneCallBacks">卸载场景回调函数集</param>
+        /// <param name="userData">用户自定义数据</param>
+        void UnloadScene(string sceneAssetName, UnloadSceneCallBacks unloadSceneCallBacks, object userData);
+
+        /// <summary>
+        /// 获取资源组是否准备完毕
+        /// </summary>
+        /// <param name="resourceGroupName">要检查的资源组名称</param>
+        /// <returns></returns>
+        bool GetResourceGroupReady(string resourceGroupName);
+
+        /// <summary>
+        /// 获取资源组资源数量
+        /// </summary>
+        /// <param name="resourceGroupName">要检查的资源组名称</param>
+        /// <returns></returns>
+        int GetResourceGroupResourceCount(string resourceGroupName);
+
+        /// <summary>
+        /// 获取资源组已准备完毕资源数量
+        /// </summary>
+        /// <param name="resourceGroupName">要检查的资源组名称</param>
+        /// <returns></returns>
+        int GetResourceGroupReadyResourceCount(string resourceGroupName);
+
+        /// <summary>
+        /// 获取资源组总大小
+        /// </summary>
+        /// <param name="resourceGroupName">要检查的资源组名称</param>
+        /// <returns></returns>
+        int GetResourceGroupTotalLength(string resourceGroupName);
+
+        /// <summary>
+        /// 获取资源组已准备完毕总大小
+        /// </summary>
+        /// <param name="resourceGroupName">要检查的资源组名称</param>
+        /// <returns></returns>
+        int GetResourceGroupTotalReadyLength(string resourceGroupName);
+
+        /// <summary>
+        /// 获取资源组准备进度
+        /// </summary>
+        /// <param name="resourceGroupName">要检查的资源组名称</param>
+        /// <returns></returns>
+        float GetResourceGroupProgress(string resourceGroupName);
     }
 }
