48579a6c023e08a0d3ca6c8900e995254526fb11
JL YAN <yy.yanjl@gmail.com>
2018-01-06 00:15:38 +0800

    添加IDataTableManager.cs   DataTableManager.cs

--- GameFrameWorkLearn/DataTable/IDataTableManager.cs
+++ GameFrameWorkLearn/DataTable/IDataTableManager.cs
@@ -37,9 +37,152 @@ namespace GameFrameWork.DataTable
         void SetResourceManager(IResourceManager resourceManager);
 
         /// <summary>
-        /// 设置
+        /// 设置数据表辅助器
         /// </summary>
-        /// <param name="dataTableHelper"></param>
+        /// <param name="dataTableHelper">数据表辅助器</param>
         void SetDataTableHelper(IDataTableHelper dataTableHelper);
+
+        /// <summary>
+        /// 加载数据表
+        /// </summary>
+        /// <param name="dataTableAssetName">数据表资源名称</param>
+        void LoadDataTable(string dataTableAssetName);
+
+        /// <summary>
+        /// 加载数据表
+        /// </summary>
+        /// <param name="dataTableAssetName">数据表资源名称</param>
+        /// <param name="userData">用户自定义数据</param>
+        void LoadDataTable(string dataTableAssetName, object userData);
+
+        /// <summary>
+        /// 是否存在数据表
+        /// </summary>
+        /// <typeparam name="T">数据表行的类型</typeparam>
+        /// <returns>是否存在数据表</returns>
+        bool HasDataTable<T>() where T : IDataRow;
+
+        /// <summary>
+        /// 是否存在数据表
+        /// </summary>
+        /// <param name="dataRowType">数据表行的类型</param>
+        /// <returns>是否存在数据表</returns>
+        bool HasDataTable(Type dataRowType);
+
+        /// <summary>
+        /// 是否存在数据表
+        /// </summary>
+        /// <typeparam name="T">数据表行的类型</typeparam>
+        /// <param name="name">数据表名称</param>
+        /// <returns>是否存在数据表</returns>
+        bool HasDataTable<T>(string name) where T : IDataRow;
+
+        /// <summary>
+        /// 是否存在数据表
+        /// </summary>
+        /// <param name="dataRowType">数据表行的类型</param>
+        /// <param name="name">数据表名称</param>
+        /// <returns>是否存在数据表</returns>
+        bool HadDataTable(Type dataRowType, string name);
+
+        /// <summary>
+        /// 获取数据表
+        /// </summary>
+        /// <typeparam name="T">数据表行的类型</typeparam>
+        /// <returns>要获取的数据表</returns>
+        IDataTable<T> GetDataTable<T>() where T : IDataRow;
+
+        /// <summary>
+        /// 获取数据表
+        /// </summary>
+        /// <param name="dataRowType">数据表行的类型</param>
+        /// <returns>要获取的数据表</returns>
+        DataTableBase GetDataTable(Type dataRowType);
+
+        /// <summary>
+        /// 获取数据表
+        /// </summary>
+        /// <typeparam name="T">数据表行的类型</typeparam>
+        /// <param name="name">数据表名称</param>
+        /// <returns>要获取的数据表</returns>
+        IDataTable<T> GetDataTable<T>(string name) where T : IDataRow;
+
+        /// <summary>
+        /// 获取数据表
+        /// </summary>
+        /// <param name="dataRowType">数据表行的类型</param>
+        /// <param name="name">数据表名称</param>
+        /// <returns>要获取的数据表</returns>
+        DataTableBase GetDataTable(Type dataRowType, string name);
+
+        /// <summary>
+        /// 获取所有数据表
+        /// </summary>
+        /// <returns>所有数据表</returns>
+        DataTableBase[] GetAllDataTables();
+
+        /// <summary>
+        /// 创建数据表
+        /// </summary>
+        /// <typeparam name="T">数据表行的类型</typeparam>
+        /// <param name="text">要解析的数据表文本</param>
+        /// <returns>要创建的数据表</returns>
+        IDataTable<T> CreateDataTable<T>(string text) where T : class, IDataRow, new();
+
+        /// <summary>
+        /// 创建数据表
+        /// </summary>
+        /// <param name="dataRowType">数据表行的类型</param>
+        /// <param name="text">要解析的数据表文本</param>
+        /// <returns>要创建的数据表</returns>
+        DataTableBase CreateDataTable(Type dataRowType, string text);
+
+        /// <summary>
+        /// 创建数据表
+        /// </summary>
+        /// <typeparam name="T">数据表行的类型</typeparam>
+        /// <param name="name">数据表名称</param>
+        /// <param name="text">要解析的数据表文本</param>
+        /// <returns>要创建的数据表</returns>
+        IDataTable<T> CreateDataTable<T>(string name, string text) where T : class, IDataRow, new();
+
+        /// <summary>
+        /// 创建数据表
+        /// </summary>
+        /// <param name="dataRowType">数据表行的类型</param>
+        /// <param name="name">数据表名称</param>
+        /// <param name="text">要解析的数据表文本</param>
+        /// <returns>要创建的数据表</returns>
+        DataTableBase CreateDataTable(Type dataRowType, string name, string text);
+
+        /// <summary>
+        /// 销毁数据表
+        /// </summary>
+        /// <typeparam name="T">数据表行的类型</typeparam>
+        /// <returns>是否销毁数据表成功</returns>
+        bool DestroyDataTable<T>() where T : IDataRow;
+
+        /// <summary>
+        /// 销毁数据表
+        /// </summary>
+        /// <param name="dataRowType">数据表行的类型</param>
+        /// <returns>是否销毁数据表成功</returns>
+        bool DestroyDataTable(Type dataRowType);
+
+        /// <summary>
+        /// 销毁数据表
+        /// </summary>
+        /// <typeparam name="T">数据表行的类型</typeparam>
+        /// <param name="name">数据表名称</param>
+        /// <returns>是否销毁数据表成功</returns>
+        bool DestroyDataTable<T>(string name) where T : IDataRow;
+
+        /// <summary>
+        /// 销毁数据表
+        /// </summary>
+        /// <param name="dataRowType">数据表行的类型</param>
+        /// <param name="name">数据表名称</param>
+        /// <returns>是否销毁数据表成功</returns>
+        bool destroyDataTable(Type dataRowType, string name);
     }
 }
