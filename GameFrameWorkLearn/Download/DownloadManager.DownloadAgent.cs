564ea426b4e2970f2060f87f63f36a4829ccd208
dianfs998 <yy.yanjl@gmail.com>
2018-01-13 18:17:14 +0800

    添加DownloadManager

---
+++ GameFrameWorkLearn/Download/DownloadManager.DownloadAgent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace GameFrameWork.Download
+{
+
+    internal partial class DownloadManager
+    {
+        /// <summary>
+        /// 下载代理
+        /// </summary>
+        private sealed class DownloadAgent : ITaskAgent<DownloadTask>, IDisposable
+        {
+            private readonly IDownloadAgentHelper m_Helper;
+            private DownloadTask m_Task;
+            private FileStream m_FileStream;
+            private int m_WaitingFlushSize;
+            private float m_WaitTime;
+            private int m_StartLength;
+            private int m_DownloadedLength;
+            private int m_SavedLength;
+            private bool m_Disposed;
+
+            public 
+        }
+    }
+}
---
+++ GameFrameWorkLearn/Download/DownloadManager.DownloadCounter.DownloadCounterNode.cs
@@ -0,0 +1,37 @@
+﻿
+
+namespace GameFrameWork.Download
+{
+    internal partial class DownloadManager
+    {
+        private partial class DownloadCounter
+        {
+            private sealed class DownloadCounterNode
+            {
+                private readonly int m_DownloadedLength;
+                private float m_ElapseSeconds;
+
+                public DownloadCounterNode(int downloadedLength)
+                {
+                    m_DownloadedLength = downloadedLength;
+                    m_ElapseSeconds = 0;
+                }
+
+                public int DownloadedLength
+                {
+                    get { return m_DownloadedLength; }
+                }
+
+                public float ElapseSeconds
+                {
+                    get { return m_ElapseSeconds; }
+                }
+
+                public void Update(float elapseSeconds, float realElapseSeconds)
+                {
+                    m_ElapseSeconds += realElapseSeconds;
+                }
+            }
+        }
+    }
+}
